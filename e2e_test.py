3792c41c15ee54619c6a9a23f61eed0b77912d4e
gapper100 <jaspermakkinje@gmail.com>
2018-11-23 10:35:03 +0100

    Update project ID and App URL

--- e2e_test.py
+++ e2e_test.py
@@ -11,7 +11,7 @@
 import urllib2
 import logging
 
-HOST='http://continuous-deployment-python.appspot.com'
+HOST='http://ci-cd-demo-with-travis-and-gcp.appspot.com/'
 
 # [START e2e]
 response = urllib2.urlopen("{}/get_author/ulysses".format(HOST))
